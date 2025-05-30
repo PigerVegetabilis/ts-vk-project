d45da3fa1ec3e5eca7613d987a095c3be9c34596
bobr <paveleagle2005@gmail.com>
2025-05-30 14:55:56 +0300

    Добавлена таблица

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -1,1 +1,43 @@
-import {Table, TableHead, TableRow, TableCell, TableBody} from '@mui/material'
+import {Table, TableHead, TableRow, TableCell, TableBody} from '@mui/material';
+import React, { useCallback } from 'react';
+import {FixedSizeList as List} from 'react-window';
+
+interface TableProps{
+    columns: string[];
+    data: any[];
+    loadMore: () => Promise<void>;
+    hasMore: boolean;
+}
+
+export default function CustomTable({columns, data, loadMore, hasMore} : TableProps){
+    const Row = useCallback(({index, style}:{index:number, style:React.CSSProperties}) => (
+        <TableRow style={style}>
+            {
+                columns.map((column) => (
+                    <TableCell key={`${index}-${column}`}>{data[index][column]}</TableCell>
+                ))
+            }
+        </TableRow>
+    ), [columns, data]);
+    return(
+        <Table>
+        <TableHead>
+            <TableRow>
+                {columns.map((column) => (
+                    <TableCell key={column}>{column}</TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            <List
+                height={600}
+                itemCount={data.length}
+                itemSize={50}
+                width="100%"  
+            >
+                {Row}
+            </List>
+        </TableBody>
+        </Table>
+    )
+}
